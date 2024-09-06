75f161c8b9026c18f986d0657366ec50f99e6e3a
LucasRoberto99 <lucasr.prof@gmail.com>
2024-09-06 12:18:25 +0200

    footer update by lucas, added LUCAS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div style={{ background: "lightgreen", color: "black", padding: "20px" }}>
-      Footer by Bastien !
+      Footer made by LUCAS and Bastien !
     </div>
   );
 }
