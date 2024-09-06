dd8933635f2b6959191aaa39d380543a44800d9e
LucasRoberto99 <lucasr.prof@gmail.com>
2024-09-06 11:38:59 +0200

    header modified on main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 
 export default function Header() {
-  return <h1 style={{ backgroundColor: "salmon" }}>Header du site de Lucas</h1>;
+  return (
+    <h1 style={{ backgroundColor: "salmon" }}>Header du professeur Lucas</h1>
+  );
 }
