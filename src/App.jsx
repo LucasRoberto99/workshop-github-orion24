cea798b3416bfb4b98e4cbacae3dfb0c6da13398
LucasRoberto99 <lucasr.prof@gmail.com>
2024-09-06 11:56:53 +0200

    added lorem

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ function App() {
     <>
       <Header />
       <p style={{ backgroundColor: "orange" }}>Ceci est une modification</p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laborum animi
+        voluptatem quidem. Et maxime libero vitae suscipit. Enim commodi sint
+        vitae laborum saepe, maxime ullam tempora accusantium ab. Quo,
+        asperiores.
+      </p>
     </>
   );
 }
